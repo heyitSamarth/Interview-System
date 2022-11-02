fc674bf9d7a1df47c89e2310690f3132011ab972
Samarth Dubey <samarthdubey49@gmail.com>
2022-11-03 03:22:13 +0530

    day 24

--- Client/project1/src/plugins/axios.js
+++ Client/project1/src/plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosInstance=axios.create({ baseURL :"http://localhost:4000"});
+const axiosInstance=axios.create({ baseURL :"http://interview-system1.herokuapp.com/"});
 
 export default axiosInstance;
--- Server/config/db.js
+++ Server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // database: "mongodb://localhost:27017/project1",
-    database: "mongodb+srv://Heyitsamarth:Password1@project1.i5cuq0m.mongodb.net/project1",
+    database: "mongodb://localhost:27017/project1",
+    // database: "mongodb+srv://Heyitsamarth:Password1@project1.i5cuq0m.mongodb.net/project1",
   };
